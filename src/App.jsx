c95c3831abd322608c30e1ec4838cbb634ce9e92
Tanisha singh sikarwar <tanishasikarwar1@gmail.com>
2026-07-21 21:41:38 +0530

    Initial ResearchOps AI setup

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      <div className="app">
+        <h1>ResearchOps AI</h1>
+
+        <p>
+          Market Research Project Management Platform
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
